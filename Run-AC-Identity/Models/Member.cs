edd628672fe09911b6cc070aa6a2b67b44fd29af
Neo Tshoma <neo.tshoma@tangentsolutions.co.za>
2024-07-24 14:45:35 +0200

    initial commit

---
+++ Run-AC-Identity/Models/Member.cs
@@ -0,0 +1,13 @@
+namespace Run_AC_Identity.Models
+{
+    public class Member
+    {
+        public string Id;
+        public string Name;
+        public string Surname;
+        public DateTime DateOfBirth;
+        public string IdNumber;
+        public string Gender;
+        public string Occupation;
+    }
+}
---
+++ Run-AC-Identity/Models/RaceEvent.cs
@@ -0,0 +1,8 @@
+namespace Run_AC_Identity.Models {
+    public class RaceEvent {
+        public string Id;
+        public string RaceName;
+        public DateTime RaceDate;
+        public List<Member> Members;
+    }
+}
